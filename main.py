7464daf74bbead63b0b4f9d369fb2bc73a0c8788
Mahdieh Mohammadi <deydad84@gmail.com>
2023-01-13 16:54:13 +0100

    hallo 4

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # This is a sample Python script.
 # Hallo Abend Lieber Mohammad
+# Hallo Abend Lieber Mohammad
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# Hallo Abend Lieber Mohammad
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
